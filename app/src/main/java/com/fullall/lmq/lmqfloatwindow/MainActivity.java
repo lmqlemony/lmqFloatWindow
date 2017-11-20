39ff34d7961f712fb663f68a7ef6fbf47448310d
lmqlemony <lmqlemony@live.com>
2017-11-20 16:33:16 +0800

    Initial commit

---
+++ app/src/main/java/com/fullall/lmq/lmqfloatwindow/MainActivity.java
@@ -0,0 +1,26 @@
+package com.fullall.lmq.lmqfloatwindow;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startFloatWindow = (Button) findViewById(R.id.start_float_window);
+        startFloatWindow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(MainActivity.this, FloatWindowService.class);
+                startService(intent);
+                finish();
+            }
+        });
+    }
+}
